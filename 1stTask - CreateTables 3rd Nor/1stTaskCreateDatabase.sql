4b1423c5c24257b1baf142a0b673c7432814dbf7
Jorgehadad <jorgemenegaz@gmail.com>
2021-12-15 22:24:38 -0300

    creating my second task

--- 1stTask - CreateTables 3rd Nor/1stTaskCreateDatabase.sql
+++ 1stTask - CreateTables 3rd Nor/1stTaskCreateDatabase.sql
@@ -1,5 +1,42 @@
 create database solvd;
 
+/* TEXT DESCRITION
+  The Minister of the Environment has decided to create an information system on the natural parks managed by each autonomous community (hereinafter, CA). 
+      After carrying out a detailed analysis, the following conclusions have been reached:
+  An autonomous community can have several natural parks. In all of CA there is one and only one body responsible for the parks. A park can be shared by more than one CA.
+  A natural park is identified by a name, it was declared on a date, it is made up of several areas identified by a name and characterized by a certain extension. For reasons of efficiency, it is desired to favor inquiries regarding the number 
+      of existing parks in each AC and the total area declared a natural park in each AC.
+  In each area there are necessarily species that can be of three types: plants, animals and minerals. Each species has a scientific name, a common name, 
+      and an inventoried number of individuals per area. Of the plant species it is desired to know if they have flowering and in what period it occurs; of the 
+      animals you want to know their type of feeding (herbivorous, carnivorous or omnivorous) and their annual heat period; of the minerals you want to know 
+      if it is crystals or rocks. In addition, it is interesting to record which species serve as food for other species, taking into account that no mineral species 
+      is considered food for any other species and that a plant species does not feed on any other species.
+
+  Park personnel keep their DNI, social security number, address, telephone numbers (home, mobile) and salary. The different types of personnel are distinguished:
+  • Management staff: records the data of park visitors and they are posted at a park entrance (entrances are identified by a number).
+  • Surveillance personnel: they monitor a certain area of ​​the park that a vehicle travels through (type and license plate).
+  • Conservation staff: maintains and conserves a certain area of ​​the park. Each one performs it in a specific specialty (cleaning, roads, etc.).
+  • Research staff: they have a qualification that must be collected and they can carry out (even jointly) research projects on a certain species, 
+    so that a researcher works on a project analyzing several species, a species can be investigated by a researcher in several projects and in a project a species 
+    can be investigated by several researchers.
+  
+  A research project has a budget and an unrealization period.
+A visitor (ID, name, address and profession) must stay within the accommodation available in the park; These have a limited capacity 
+    and are of a certain category.
+The accommodations organize excursions to the park, in a vehicle or on foot, on certain days of the week and at a certain time. 
+    Any visitor to the park can attend these excursions.
+*/
+
+
+
+/*GLOSSARY*/
+/*
+  NombreP: Is the name of the park
+  NroAlojamiento: Is the number of the accommodation
+
+*/
+
+
 /*the action between a Visitant of the Park and 
 with the place where they sleep*/
 CREATE TABLE `aloja` (
