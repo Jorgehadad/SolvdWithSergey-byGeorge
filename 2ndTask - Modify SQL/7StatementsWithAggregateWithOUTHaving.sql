572f98c57304620b7f229868221a89a67e125210
Jorgehadad <jorgemenegaz@gmail.com>
2021-12-19 19:21:27 -0300

    fixing my 7StatementsWithAggregateWithout having

--- 2ndTask - Modify SQL/7StatementsWithAggregateWithOUTHaving.sql
+++ 2ndTask - Modify SQL/7StatementsWithAggregateWithOUTHaving.sql
@@ -1,15 +1,31 @@
+#count how many staff are in the database
 select count(*)
-from personal
-group by Nombre;
+from staff;
 
+#show maximun staff salary
 select max(sueldo)
-from personal
-group by sueldo;
+from personal;
 
+#show average staff salary
 select avg(sueldo)
-from personal
-group by sueldo;
+from personal;
 
-select min(sueldo)
-from personal
-group by sueldo;
+#show minimum staff salary
+select min(salary)
+from staff;
+
+
+select count(*) as NumberofExcursions
+from excursions
+where excursions.date > "2020-10-10";
+
+#Count how many parks are without consider the park "With Summer"
+SELECT count(*)
+FROM `solvd`.`parks`
+where NameP != "With Summer" ;
+
+
+#Show the average of the all of the proyects after "2020-01-01"
+select avg(budget)
+from proyect
+where startDate > "2020-01-01";
