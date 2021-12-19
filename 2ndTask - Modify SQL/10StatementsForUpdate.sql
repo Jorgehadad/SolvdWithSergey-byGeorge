d9322dda01e40b3d444e02395af8b563a92781ab
Jorgehadad <jorgemenegaz@gmail.com>
2021-12-19 18:34:42 -0300

    fixing my 10Statementsforupdate

--- 2ndTask - Modify SQL/10StatementsForUpdate.sql
+++ 2ndTask - Modify SQL/10StatementsForUpdate.sql
@@ -1,31 +1,47 @@
 UPDATE `parques`
 SET `NombreP` = "Park1"
-WHERE `NombreP` = "Con Privamera";
-UPDATE `parques`
-SET `NombreP` = "Park2"
-WHERE `NombreP` = "Con Privamera";
-UPDATE `parques`
-SET `NombreP` = "Park3"
-WHERE `NombreP` = "Con Privamera";
-UPDATE `parques`
-SET `NombreP` = "Park4"
-WHERE `NombreP` = "Con Privamera";
-UPDATE `parques`
-SET `NombreP` = "Park5"
-WHERE `NombreP` = "Con Privamera";
-UPDATE `parques`
-SET `NombreP` = "Park6"
-WHERE `NombreP` = "Con Privamera";
-UPDATE `parques`
-SET `NombreP` = "Park7"
-WHERE `NombreP` = "Con Privamera";
-UPDATE `parques`
-SET `NombreP` = "Park8"
-WHERE `NombreP` = "Con Privamera";
-UPDATE `parques`
-SET`NombreP` = "Park9"
-WHERE `NombreP` = "Con Privamera";
-UPDATE `parques`
-SET `NombreP` = "Park10"
-WHERE `NombreP` = "Con Privamera";
+WHERE `NombreP` = "With Spring";
+
+UPDATE `solvd`.`accomodation`
+SET `Capacity` = 10
+WHERE `NumAccomodation` = 1 AND `NameP` = "Day Light";
+
+UPDATE `solvd`.`accomodation`
+SET `Capacity` = 5
+WHERE accomodation.Capacity <= 2;
+
+UPDATE `solvd`.`animals`
+SET `feeding` = "vegetables"
+WHERE `scientific_name` = "Juampus";
+
+UPDATE `solvd`.`areas`
+SET `Extension` = 500
+WHERE `NameA` = "Area1" AND `NameP` = "With Summer";
+
+UPDATE `solvd`.`housed`
+SET `Room` = "Room 10"
+WHERE `NumAccomodation` = 342 AND `dniVisitor` = 42000222;
+
+UPDATE `solvd`.`vegetables`
+SET `Flowering` = 1
+WHERE `scientific_name` = "flowercina";
+
+UPDATE `solvd`.`sale` 
+SET `Date_payment` = '2022-10-20 11:20:38' 
+WHERE (`DNI` = '10057482') and (`NumTicket` = '1') and (`NameP` = 'With Spring');
+
+UPDATE `solvd`.`enjoy` 
+SET `Cod_Excursion` = '48' 
+WHERE (`Cod_Excursion` = '47') and (`DNI` = '18351435');
+
+UPDATE `solvd`.`excursions` 
+SET `Hour` = '00:05:00' 
+WHERE (`Cod_Excursion` = '1');
+
+
+
+
+
+
+
 
